a688afb2d25cebb25e6bfcba61f822be167e633e
Kubikk <IvanAniskov15@gmail.com>
2021-05-19 00:00:13 +0300

    HW1: Ivan Aniskov

---
+++ src/task_1_1.py
@@ -0,0 +1,40 @@
+# Задание 1
+a = 5
+b = 2
+summa = a + b
+div = a / b
+mult = a * b
+print("Сумма равна:", summa)
+print("Разность равна:", div)
+print("Произведение равно:", mult)
+
+# Задание 2 Сделал так, потому что через input постоянно просило вводить числа
+x = 6
+y = -8
+import math
+
+result = (math.fabs(x) - math.fabs(y)) / (1 - math.fabs(x * y))
+print("Result is: ", result)
+
+# Задание 3
+L = 3
+S = math.pow(L, 2)
+V = math.pow(L, 3)
+print("Square:", S)
+print("Volume:", V)
+
+# Задание 4
+num_1 = 6
+num_2 = 7
+arith = (num_1 + num_2) / 2
+geom = math.sqrt(num_1 * num_2)
+print("Average:", arith)
+print("Geometric mean:", geom)
+
+# Задание 5
+a = 6
+b = 10
+c = math.sqrt(math.pow(a, 2) + math.pow(b, 2))
+s = a * b / 2
+print("Hypotenuse:", c)
+print("Square:", s)
---
+++ src/task_2_2.py
@@ -0,0 +1,24 @@
+# Task 1
+string = "Hello Python"
+string_result = string[2]
+print(string_result)
+
+# Task 2
+string_result = string[-2]
+print(string_result)
+
+# Task 3
+string_result = string[0: 5]
+print(string_result)
+
+# Task 4
+string_result = string[: -2]
+print(string_result)
+
+# Task 5
+string_result = ""
+i = 0
+while i < len(string):
+    string_result = string_result + string[i]
+    i = i + 2
+print(string_result)
