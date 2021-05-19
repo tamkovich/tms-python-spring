d9772c262d73e95b59ffbdb07b08742df6b60411
gleb samoylov <g1ebyshka897@gmail.com>
2021-05-19 20:01:20 +0300

    HW3:Samoylov gleb

---
+++ src/task_2_2.py
@@ -0,0 +1,7 @@
+st = 'anastasia mila ramy'
+hw2_1 = st[2]
+hw2_2 = st[-2]
+hw2_3 = st[0: 5]
+hw2_4 = st[0: -2]
+hw2_5 = st[0::2]
+print(F" {st} \n {hw2_1} \n {hw2_2} \n {hw2_3} \n {hw2_4} \n {hw2_5}")
