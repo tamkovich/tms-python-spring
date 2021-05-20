4cb99206b35c4ede7182b540a0b105669c77603e
Kate Tishevich <katsiaryna.t@upservice.io>
2021-05-20 14:35:03 +0300

    pep8

--- src/task_1_5.py
+++ src/task_1_5.py
@@ -1,14 +1,16 @@
 import math
+
 from fractions import Fraction
+
 
 a = 5
 b = 7
 
-c = math.sqrt((a**2)+(b**2))
+c = math.sqrt((a**2) + (b**2))
 print(c)
 
 
-after_review_c = ((a**2)+(b**2))**0.5
+after_review_c = ((a**2) + (b**2))**0.5
 print(after_review_c)
 
 
