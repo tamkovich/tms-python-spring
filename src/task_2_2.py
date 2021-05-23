ad479f2a9ebc5102c5cbea38f0ee9969d40b508c
Ivan Aniskov <ivananiskov15@gmail.com>
2021-05-23 23:19:09 +0300

    HW3: Ivan Aniskov

--- src/task_2_2.py
+++ src/task_2_2.py
@@ -4,18 +4,22 @@ string_result = string[2]
 print(string_result)
 
 # Task 2
+string = "Hello Python"
 string_result = string[-2]
 print(string_result)
 
 # Task 3
+string = "Hello Python"
 string_result = string[0: 5]
 print(string_result)
 
 # Task 4
+string = "Hello Python"
 string_result = string[: -2]
 print(string_result)
 
 # Task 5 Решил использовать этот метод, так как это проще всего)
+string = "Hello Python"
 string_result = ""
 i = 0
 while i < len(string):
