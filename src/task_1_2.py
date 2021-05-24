b218027aa4a439532bc9be8af84c8b456e05ca08
pevlens <pevlens1501@gmail.com>
2021-05-24 14:25:34 +0300

    Update_3 HW<1-3> Siarhei Polyuan

--- src/task_1_2.py
+++ src/task_1_2.py
@@ -1,5 +1,5 @@
 from math import fabs
 x = -4.5
 y = 9.7
-composition = (fabs(x) - fabs(y)) / (1 + fabs(x*y))
+composition = (fabs(x) - fabs(y)) / (1 + fabs(x * y))
 print(composition)
--- src/task_1_4.py
+++ src/task_1_4.py
@@ -1,4 +1,4 @@
-last = (1 , 2 , 5 , 8 , 1 ,5 , 2 , 6 , 3)
+last = (1, 2, 5, 8, 1, 5, 2, 6, 3)
 arithmetical_mean = sum(last) / len(last)
 product_numbers_1 = 1
 for i in range(len(last)):
