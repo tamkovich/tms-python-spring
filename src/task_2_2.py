2bae6f629fb7a89c987bf70579cd418fa8098948
Ivan Aniskov <ivananiskov15@gmail.com>
2021-05-24 23:00:09 +0300

    HW2: Ivan Aniskov

--- src/task_2_2.py
+++ src/task_2_2.py
@@ -1,28 +1,4 @@
-# Task 1
-string = "Hello Python"
-string_result = string[2]
-print(string_result)
-
-# Task 2
+# Task 2 Создать строку равную предпоследнему символу введенной строки
 string = "Hello Python"
 string_result = string[-2]
-print(string_result)
-
-# Task 3
-string = "Hello Python"
-string_result = string[0: 5]
-print(string_result)
-
-# Task 4
-string = "Hello Python"
-string_result = string[: -2]
-print(string_result)
-
-# Task 5 Решил использовать этот метод, так как это проще всего)
-string = "Hello Python"
-string_result = ""
-i = 0
-while i < len(string):
-    string_result = string_result + string[i]
-    i = i + 2
 print(string_result)
