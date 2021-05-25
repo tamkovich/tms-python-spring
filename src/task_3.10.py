0fc6dbe3c23aa1d81d8672037d815c811339f6d3
Egor <Yegor.lob@yandex.ru>
2021-05-25 15:12:46 +0300

    task 3.10 complete finish fixing "7 weak warnings"

--- src/task_3.10.py
+++ src/task_3.10.py
@@ -1,7 +1,13 @@
 # Получить на ввод количество рублей и копеек и вывести в правильной
 # форме, например, 3 рубля, 1 рубль 25 копеек, 3 копейки
 
-money = input('Enter your cash: ')
+money = input('Sample input: 2,31\n'
+              '              5,86\n'
+              '              0,44\n'
+              'Enter your cash: ')
+
+#
+
 if '.' or ',' in money:
     money = money.replace(',', '.').split('.')
 
@@ -9,13 +15,13 @@ rub = ''
 coin = ''
 
 if money[0] != '0':
-    if int(money[0]) >= 5 and int(money[0]) <= 20:
+    if 5 <= int(money[0]) <= 20:
         rub = money[0] + ' рублей'
     elif int(money[0]) % 10 == 1:
         rub = money[0] + ' рубль'
-    elif int(money[0]) % 10 >= 2 and int(money[0]) % 10 <= 4:
+    elif 2 <= int(money[0]) % 10 <= 4:
         rub = money[0] + ' рубля'
-    elif int(money[0]) % 10 >= 5 and int(money[0]) % 10 <= 9:
+    elif 5 <= int(money[0]) % 10 <= 9:
         rub = money[0] + ' рублей'
     elif int(money[0]) % 10 == 0:
         rub = money[0] + ' рублей'
@@ -24,16 +30,16 @@ else:
 
 try:
     if money[1] != '':
-        if int(money[1]) >= 5 and int(money[1]) <= 20:
+        if 5 <= int(money[1]) <= 20:
             coin = money[1] + ' копеек'
             print(rub, coin)
         elif int(money[1]) % 10 == 1:
             coin = money[1] + ' копейка'
             print(rub, coin)
-        elif int(money[1]) % 10 >= 2 and int(money[1]) % 10 <= 4:
-            coin = money[0] + ' копейки'
+        elif 2 <= int(money[1]) % 10 <= 4:
+            coin = money[1] + ' копейки'
             print(rub, coin)
-        elif int(money[1]) % 10 >= 5 and int(money[1]) % 10 <= 9:
+        elif 5 <= int(money[1]) % 10 <= 9:
             coin = money[1] + ' копеек'
             print(rub, coin)
         elif int(money[1]) % 10 == 0:
@@ -43,4 +49,3 @@ try:
         pass
 except IndexError:
     print(rub)
-
