e976d19dfbdb40c7b84d6da3d5f9b81754d25df6
yosu <yosu_95@inbox.ru>
2021-05-25 18:34:51 +0500

    HW4: Kasimova Yosuman

--- src/task_3_1.py
+++ src/task_3_1.py
@@ -1,4 +1,7 @@
 # Введите число. Если это число делиться на 1000 без остатка, то выведите на экран "millennium"
-number = int(input("Введите число:"))
-if number % 1000 == 0:
-    print("millennium")
+number = input("Введите число: ")
+if number.isdigit():
+    if int(number) % 1000 == 0:
+        print("millennium")
+else:
+    print("Не было введено число!")
