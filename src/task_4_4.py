6e6d2ee55894c87316af9ea9d1baffffea123019
yosuman95 <77063637+yosuman95@users.noreply.github.com>
2021-05-26 21:22:37 +0500

    Update task_4_4.py

--- src/task_4_4.py
+++ src/task_4_4.py
@@ -3,15 +3,15 @@ print("Используем цикл While: ")
 spisok = [1, 2, 3, 4, 5]
 i = 0
 x = spisok[0]
-while i < (len(spisok)-1):
-    spisok[i] = spisok[i+1]
+while i < (len(spisok) - 1):
+    spisok[i] = spisok[i + 1]
     i += 1
-spisok[len(spisok)-1] = x
+spisok[len(spisok) - 1] = x
 print(spisok)
 print("\nИспользуем цикл For: ")
 spisok = [1, 2, 3, 4, 5]
 x = spisok[0]
-for i in range(len(spisok)-1):
+for i in range(len(spisok) - 1):
     spisok[i] = spisok[i + 1]
     i += 1
 spisok[len(spisok) - 1] = x
