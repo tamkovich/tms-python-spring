0d8969577474a5f316826c47e8d65276570205f0
ivan1402 <61510ivan>
2021-05-27 12:53:36 +0300

    HW1: Ivan Kovalevich

---
+++ src/task_1_4.py
@@ -0,0 +1,8 @@
+""" Даны два действительных числа.
+Найти среднее арифметическое и среднее геометрическое этих чисел."""
+
+x = 3
+y = -2
+arithmetic_mean = (x + y) / 2
+geometric_mean = (x*y) ** (1 / 2)
+print(arithmetic_mean, geometric_mean)
