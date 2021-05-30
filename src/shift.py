b813a22baf5f61d1462079de82da325aab4a7acc
Dmitry Kolyago <kaliaha@icloud.com>
2021-05-30 17:53:08 +0300

    Fixed "is_ntersect" method in class "Shift".

--- src/shift.py
+++ src/shift.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+
+
 class Shift:
     def __init__(self, shift_info):
         self.time_from = shift_info['time_from']
@@ -7,20 +10,38 @@ class Shift:
         self.week_days = shift_info['week_days']
 
     def is_intersect(self, shift_2):
-
         early_shift = self if self.date_from < shift_2.date_from else shift_2
         late_shift = self if self.date_from >= shift_2.date_from else shift_2
+
+        start, end = early_shift.time_from.hour, early_shift.time_to.hour
+        if start >= end:
+            early_shift.date_to += timedelta(1)
+
+        start, end = late_shift.time_from.hour, late_shift.time_to.hour
+        if start >= end:
+            late_shift.date_to += timedelta(1)
 
         is_date_intersect = early_shift.date_from <= late_shift.date_from <= early_shift.date_to
         if not is_date_intersect:
             return False
 
-        is_dow_intersect = not set(self.week_days).isdisjoint(set(shift_2.week_days))
+        self_dow = set(self.week_days)
+        start, end = self.time_from.hour, self.time_to.hour
+        if start >= end:
+            for day in self.week_days:
+                self_dow.add(day + 1 if day != 7 else 1)
+
+        shift_2_dow = set(shift_2.week_days)
+        start, end = shift_2.time_from.hour, shift_2.time_to.hour
+        if start >= end:
+            for day in shift_2.week_days:
+                shift_2_dow.add(day + 1 if day != 7 else 1)
+
+        is_dow_intersect = not self_dow.isdisjoint(shift_2_dow)
         if not is_dow_intersect:
             return False
 
         hours = list(range(24))
-
         start, end = self.time_from.hour, self.time_to.hour
         if start >= end:
             self_hours = set(hours[start:] + hours[:end])
