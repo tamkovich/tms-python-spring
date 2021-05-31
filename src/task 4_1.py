0f55a9d1772085743b96506247abdc9f5405d417
Ivan_Vetrovsky <vanek-bortnik@mail.ru>
2021-05-31 11:26:38 +0300

    fix_1 (пробелы)

--- src/task 4_1.py
+++ src/task 4_1.py
@@ -10,12 +10,12 @@ c = 0
 a = -2
 b = 0
 while True:
-   if b <= len(list_1) - 1:
-       c = a * list_1[b]
-       b += 1
-       list_new.append(c)
-   else:
-      break
+    if b <= len(list_1) - 1:
+        c = a * list_1[b]
+        b += 1
+        list_new.append(c)
+    else:
+        break
 print(list_new)
 
 # for:
@@ -23,7 +23,7 @@ print('Вариант с for:')
 list_new = []
 list_1 = [1, 2, 4, 5]
 for i in list_1:
-   a = -2
-   c = a * i
-   list_new.append(c)
+     a = -2
+     c = a * i
+     list_new.append(c)
 print(list_new)
