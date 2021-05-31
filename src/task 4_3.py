f66c410e8aa1152586e3209185430c771998f6c5
Ivan_Vetrovsky <vanek-bortnik@mail.ru>
2021-05-31 11:19:00 +0300

    HW4

---
+++ src/task 4_3.py
@@ -0,0 +1,42 @@
+# Задание 4_3
+# Дан словарь: {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
+# Добавить каждому ключу число равное длине этого ключа
+# (пример {'key': 'value'} -> {'key3': 'value'})
+# Чтобы получить список ключей - использовать в конце метод .keys()
+# (подсказка: создаётся новый ключ с цифрой в конце, старый удаляется)
+
+# while:
+print('Вариант с while:')
+dict_1 = {'test': 'test_value',
+          'europe': 'eur',
+          'dollar': 'usd',
+          'ruble': 'rub'}
+dict_keys = dict_1.keys()
+dict_values = dict_1.values()
+keys = list(dict_keys)
+values = list(dict_values)
+i = 0
+while i < len(keys):
+    key = keys[i]
+    del dict_1[key]
+    dict_1[key + str(len(key))] = values[i]
+    i += 1
+print(dict_1)
+
+# for:
+print('Вариант с for:')
+dict_1 = {'test': 'test_value',
+          'europe': 'eur',
+          'dollar': 'usd',
+          'ruble': 'rub'}
+dict_keys = dict_1.keys()
+dict_values = dict_1.values()
+keys = list(dict_keys)
+values = list(dict_values)
+i = 0
+for i in range(len(keys)):
+    key = keys[i]
+    del dict_1[key]
+    dict_1[key + str(len(key))] = values[i]
+    i += 1
+print(dict_1)
