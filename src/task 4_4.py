746525299577aac35f4a62b9c794995a015ea4f1
Ivan_Vetrovsky <vanek-bortnik@mail.ru>
2021-05-31 11:45:45 +0300

    HW 4

---
+++ src/task 4_4.py
@@ -0,0 +1,23 @@
+# Задание 4_4
+# Дан список
+# Создать новый список, сдвинутый на 1 элемент влево
+# Пример: 1 2 3 4 5 -> 2 3 4 5 1
+
+# c while:
+print('Вариант с while:')
+list = [1, 2, 3, 4, 5, 6, 7]
+list_new = []
+i = 0
+while i < len(list):
+    num = list.pop()
+    list_new.append(num)
+print(list_new)
+
+# c for:
+print('Вариант с for:')
+list = [1, 2, 3, 4, 5, 6, 7]
+list_new = []
+for i in range(len(list)):
+    num = list.pop()
+    list_new.append(num)
+print(list_new)
