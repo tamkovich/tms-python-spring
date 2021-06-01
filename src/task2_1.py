b5ddb243812fe11012c5889bba8b1d421aaa79d3
Kate Tishevich <katsiaryna.t@upservice.io>
2021-06-01 13:19:15 +0300

    fixed comments of my hw

--- src/task2_1.py
+++ src/task2_1.py
@@ -1,9 +1,16 @@
+# Создать переменные firstname, lastname, age с соответствующими значениями
+# Путем сложения получить строку вида
+# Привет, меня зовут Иван Иванов, мне 35 лет
+# Примечание:
+# Переменную age задавать как строку ‘35’
+
+
 firstname = 'Kate'
 
 lastname = 'Tishevich'
 
 age = '22'
 
-result = ("Хей,я" + " " + firstname + " " + lastname + "," + "мне" + " " + age + " " + "года")
+result = f"Хей,я" + " " + firstname + " " + lastname + "," + "мне" + " " + age + " " + "года"
 
 print(result)
