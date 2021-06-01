6b7254f144306ce183bc5abf028149b59fc09f03
yosu <yosu_95@inbox.ru>
2021-06-01 19:49:04 +0500

    HW5: Kasimova Yosuman

--- src/task_4_1.py
+++ src/task_4_1.py
@@ -4,13 +4,14 @@
 """
 print("Используем цикл While: ")
 number = [2, 12, -7, 16, 8]
-number_new = list(range(len(number)))
+number_new = []
 i = 0
 while i < len(number):
-    number_new[i] = number[i] * (-2)
+    number_new.append(number[i] * -2)
     i += 1
 print(number_new)
+number_new = []
 print("\nИспользуем цикл For: ")
 for i in range(len(number)):
-    number_new[i] = number[i] * (-2)
+    number_new.append(number[i] * -2)
 print(number_new)
