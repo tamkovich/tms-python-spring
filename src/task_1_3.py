337829080fb96cf6e003d851451c5b0dc1d6c139
Ivan Kovalevich <84102046+ivan1402@users.noreply.github.com>
2021-06-01 15:09:25 +0300

    Delete task_1_3.py

--- src/task_1_3.py
+++
@@ -1,7 +0,0 @@
-""" Дана длина ребра куба. Найти объем куба и площадь его боковой
-поверхности."""
-
-long_cube_adge = 2
-cube_volume = long_cube_adge ** 3
-cube_area_adge = long_cube_adge ** 2
-print(cube_volume, cube_area_adge)
