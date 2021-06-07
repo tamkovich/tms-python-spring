d0bcb60593f3f0856c18d8c497e9c7d37438a7dc
Egor <Yegor.lob@yandex.ru>
2021-06-07 20:52:03 +0300

    HW_8 fix fucking tox #1

--- src/functions.py
+++ src/functions.py
@@ -1,7 +1,7 @@
 # task_8_01
 # my first test recursive function
 def double_recurs_factorial(n: int) -> int:
-    return n if n == 1 or n == 2 else n*double_recurs_factorial(n-2)
+    return n if n == 1 or n == 2 else n * double_recurs_factorial(n - 2)
 
 
 # task_8_02
