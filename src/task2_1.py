a800a47b734b01aaad6e9e00f64eaf8ffd96c674
Kate Tishevich <katsiaryna.t@upservice.io>
2021-06-07 21:03:31 +0300

    HW 5: Kate Tishevich

--- src/task2_1.py
+++ src/task2_1.py
@@ -11,6 +11,6 @@ lastname = 'Tishevich'
 
 age = '22'
 
-result = ("Хей,я" + " " + firstname + " " + lastname + "," + "мне" + " " + age + " " + "года")
+result = f"Хей,я {firstname} {lastname} мне {age} года"
 
 print(result)
