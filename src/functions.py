b9f36e2457ae558212c20ee6fa322171f277874e
Egor <Yegor.lob@yandex.ru>
2021-06-07 20:48:14 +0300

    HW_8 complete

---
+++ src/functions.py
@@ -0,0 +1,9 @@
+# task_8_01
+# my first test recursive function
+def double_recurs_factorial(n: int) -> int:
+    return n if n == 1 or n == 2 else n*double_recurs_factorial(n-2)
+
+
+# task_8_02
+def polindr(n: str):
+    print(n) if n == n[:: -1] else n
