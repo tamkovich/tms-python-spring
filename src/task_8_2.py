d5272ddaf91a4d31e1daf654720779fdb69ad8ed
pevlens <pevlens1501@gmail.com>
2021-06-07 20:35:29 +0300

    HW 8:Serhei_polyuan

---
+++ src/task_8_2.py
@@ -0,0 +1,10 @@
+
+def polindrom(word):
+    return word == word[::-1]
+
+if __name__ == '__main__':
+    word = "дед"
+    if polindrom(word):
+        print("слово полиндром")
+    else:
+        print("слово не является полиндромом")
---
+++ src/task_8_3.py
@@ -0,0 +1,15 @@
+def fact(number): #функция факториала
+    prz = 1
+    for value in range(1,number):
+        prz *= value 
+    return prz
+
+def Sin1(x: float, n: float) -> float: # Задаем тип параметров вещественные и возврат фунции вещественый
+    # создаем слогаемые n > 0
+    list_slogaemie  = [(-1)**value * x**(2 * value + 1) / fact(2 * value + 1) for value in range(int(n)) if value > 0]
+    # формируемм список слогаемых больше n
+    sum_ = sum([value for value in list_slogaemie if value > n])
+    # возвращаем приблизитеьное значение sin(x)
+    return (x - x**(3 / fact(3)) + x**(5 / fact(5))) - sum_
+
+print(Sin1(10, 11))
