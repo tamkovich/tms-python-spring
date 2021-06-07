45f3b4ec7f491ebec25b4b96028b3cac14fe1134
pevlens <pevlens1501@gmail.com>
2021-06-07 20:37:13 +0300

    HW 8:Serhei_polyuan update_2

--- src/task_8_1.py
+++ src/task_8_1.py
@@ -1,15 +1,15 @@
 from typing import NewType
 
-n_type  = NewType('n' , int) # параметр целого типа 
-def fact2 (n: n_type) -> int:
+n_type = NewType("n", int)  # параметр целого типа
+
+
+def fact2(n: n_type) -> int:
     factrial_nummber = 1
     if n % 2 == 0:
         fact2_list = [number for number in range(1, n) if number % 2 == 0]
     else:
         fact2_list = [number for number in range(1, n) if number % 2 != 0]
 
-    for  value in fact2_list:
+    for value in fact2_list:
         factrial_nummber *= value
     return factrial_nummber
-
-
--- src/task_8_2.py
+++ src/task_8_2.py
@@ -1,8 +1,8 @@
-
 def polindrom(word):
     return word == word[::-1]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     word = "дед"
     if polindrom(word):
         print("слово полиндром")
