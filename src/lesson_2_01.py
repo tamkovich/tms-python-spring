49b677be12a7f6094be42bb43f5fc5d5415e1fe5
Nikita Semiachka <sendtonik@mail.ru>
2021-06-08 10:14:23 +0300

    HW1: Nikita Semiachka

---
+++ src/lesson_2_01.py
@@ -0,0 +1,19 @@
+# Задание 2.01
+# Создать переменные firstname, lastname, age с соответствующими значениями
+# Путем сложения получить строку вида Привет, меня зовут Иван Иванов, мне 35 лет
+# Примечание:Переменную age задавать как строку ‘35’
+firstname = "Никита"
+lastname = " Семячко"
+age = "26"
+i = "Привет, меня  зовут " + firstname + lastname + ", " + "мне " + age + " лет"
+print(i)
+print(len(i))
+print(age.isdigit())
+print(i.isdigit())
+print(i.find("меня"))
+print(firstname[3:])
+k = "Hello world"
+print(k[0:7:3])
+print(lastname[0: 5:2])
+stroka ="My {} {} {}".format("Hello" ,"Sweet", "World")
+print(stroka)
---
+++ src/lesson_2_02.py
@@ -0,0 +1,15 @@
+# Задание 2.02
+# Создать два списка произвольного содержания.
+# Добавить к каждому списку по одному элементу в конец и в начало.
+# Расширить первый список вторым.
+# Все изменения выводить на экран.
+songs = ["creep", "hurricane", "war"]
+music = ["rock", "rnb", "dnb"]
+songs.insert(0, "pretending")
+songs.append("cry")
+music.append("metal")
+music.insert(0, "pop")
+print(songs)
+print(music)
+songs.extend(music)
+print(songs)
