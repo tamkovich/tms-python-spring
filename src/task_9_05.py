ab10f5e60cdec53111551ba19d9be37f9c3589f8
g1ebyshka <84071147+g1ebyshka@users.noreply.github.com>
2021-06-09 15:39:10 +0300

    Update task_9_05.py

--- src/task_9_05.py
+++ src/task_9_05.py
@@ -3,7 +3,7 @@
 чисел, которые кратны 3."""
 
 from functools import reduce
-nambers = [1,2,3,6,9]
+nambers = [1, 2, 3, 6, 9]
 re = list(filter(lambda a: a % 3 == 0, nambers))
 result = reduce(lambda b, x: b * x, re)
 print(result)
