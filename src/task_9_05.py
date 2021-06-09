08a4de27cf75cbcdb1512ae882a7013a0777aa0f
gleb samoylov <g1ebyshka897@gmail.com>
2021-06-09 15:34:49 +0300

    hw9:gleb_samoylov

---
+++ src/task_9_05.py
@@ -0,0 +1,9 @@
+"""Задание 9.05
+Дан список чисел. Найти произведение всех
+чисел, которые кратны 3."""
+
+from functools import reduce
+nambers = [1,2,3,6,9]
+re = list(filter(lambda a: a % 3 == 0, nambers))
+result = reduce(lambda b, x: b * x, re)
+print(result)
