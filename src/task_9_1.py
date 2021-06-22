b0fdb8bf26d783a044a693796095be56ce55924c
Ivan Aniskov <ivananiskov15@gmail.com>
2021-06-22 13:55:10 +0300

    HW11: Ivan Aniskov

--- src/task_9_1.py
+++
@@ -1,8 +0,0 @@
-"""
-Дан список строк. Отформатировать все строки в формате ‘{i} - {string}’,
-где i это порядковый номер строки в списке. Использовать генератор списков.
-"""
-
-list_elements = ['krokodil', 'cat', 'dog']
-new_list = (lambda j: [f'{i} - {list_elements}' for i, list_elements in enumerate(j, start=1)])
-print(list(new_list(list_elements)))
