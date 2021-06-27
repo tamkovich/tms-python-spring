ad0c741abed2f0c780787efa7ce0f5bfd0936cc9
KarolinaLeonovich <karolinaleonovich@gmail.com>
2021-06-27 15:13:12 +0300

    HW12 KarolinaLeonovich

---
+++ src/KarolinaLeonovich_HW_12_1(class_My_Time).py
@@ -0,0 +1,113 @@
+# Создать класс MyTime. Атрибуты: hours, minutes, seconds. Методы:
+# переопределить магические методы сравнения(==, !=, >=, <=, <, >),
+# сложения, вычитания, умножения на число, вывод на экран. Перегрузить
+# конструктор на обработку входных параметров вида: одна строка, три
+# числа, другой объект класса MyTime, и отсутствие входных параметров.
+# Реализовать нормальное отображение времени(12:65:83 - 13:06:23)
+# [my-oop-02] Задание доделать в рамках cw12
+
+class TimeMultException(Exception):
+    def __init__(self, message='time can only be multiplied by a number'):
+         super().__init__(message)
+
+class MyTime:
+    """Accepts data as input:
+     1) str type, format: "19 1 25"
+     2) int type, format: 6, 12, 14
+     3) lack of input data (creates time 0:0:0)
+     4) another MyTime object
+     If the input contains more than 60 minutes
+     or seconds, the time is recalculated.
+     You lost hours more than 24 (object contains no days).
+     So time 25:05:05 == 1:05:05
+     """
+    def __init__(self, *args):
+        if args:
+            if len(args) == 3:
+                self.hours = args[0]
+                self.minutes = args[1]
+                self.seconds = args[2]
+            elif len(*args) == 1001:
+                self.hours = args[0].hours
+                self.minutes = args[0].minutes
+                self.seconds = args[0].seconds
+            elif len(args) == 1:
+                self.hours = int(args[0].split()[0])
+                self.minutes = int(args[0].split()[1])
+                self.seconds = int(args[0].split()[2])
+            self.minutes = self.minutes + self.seconds // 60
+            self.seconds = self.seconds % 60
+            self.hours = (self.hours + self.minutes // 60) % 24
+            self.minutes = self.minutes % 60
+            self.clean_seconds = self.seconds + self.minutes*60 + self.hours*60*60
+        else:
+            self.hours = 0
+            self.minutes = 0
+            self.seconds = 0
+
+    def __str__(self):
+        return f"{self.hours}:{self.minutes}:{self.seconds}"
+
+    def __eq__(self, other):
+        return self.clean_seconds == other.clean_seconds
+
+    def __ne__(self, other):
+        return self.clean_seconds != other.clean_seconds
+
+    def __lt__(self, other):
+        return self.clean_seconds < other.clean_seconds
+
+    def __gt__(self, other):
+        return self.clean_seconds > other.clean_seconds
+
+    def __le__(self, other):
+        return self.clean_seconds <= other.clean_seconds
+
+    def __ge__(self, other):
+        return self.clean_seconds >= other.clean_seconds
+
+    def __len__(self):
+        return 1001
+
+    def __add__(self, other):
+        sum_amount = self.clean_seconds + other.clean_seconds
+        return f"{sum_amount // 3600 % 24}:{sum_amount //60 % 60}:{sum_amount % 60}"
+
+    def __sub__(self, other):
+        sub_amount = abs(self.clean_seconds - other.clean_seconds)
+        return f"{sub_amount // 3600 % 24}:{sub_amount //60 % 60}:{sub_amount % 60}"
+
+    def __mul__(self, other):
+        if type(other) != int and type(other) != float:
+            raise TimeMultException
+        else:
+            mul_amount = self.clean_seconds * other
+            return f"{mul_amount // 3600 % 24}:{mul_amount // 60 % 60}:{mul_amount % 60}"
+
+# time1 = MyTime("89 99 64")
+# print(time1)
+# print(time1*2)
+
+#
+# time2 = MyTime(24, 5, 5)
+# print(time2)
+#
+# time3 = MyTime("24 5 5")
+# print(time3)
+#
+# time4 = MyTime(time3)
+# print(time4)
+#
+# print(time1 > time2, "18;40 БОЛЬШЕ 0;5")
+# print(time1 < time2, "18;40 МЕНЬШЕ 0;5")
+# print(time1 <= time2, "18;40 МЕНЬШЕ ИЛИ РАВНО 0;5")
+# print(time1 >= time2, "18;40 БОЛЬШЕ ИЛИ РАВНО 0;5")
+# print(time1 == time2, "18;40 РАВНО 0;5")
+# print(time1 != time2, "18 40 НЕ РАВНО 0;5")
+# print(time3 != time2, "0;5 НЕ РАВНО 0;5")
+# print(time3 == time2, "0;5 РАВНО 0;5")
+#
+# print(time1+time2)
+# print(time1-time2)
+# print(time2-time1)
+# print("18 34 59")
---
+++ src/classes.py
@@ -0,0 +1,120 @@
+# Создать класс Point, описывающий точку(атрибуты: x, y). Создать класс
+# Figure. Создать три дочерних класса Circle(атрибуты: координаты
+# центра(тип Point), длина радиуса; методы: нахождение периметра и
+# площади окружности), Triangle(атрибуты: три точки, методы: нахождение
+# площади и периметра), Square(атрибуты: две точки, методы: нахождение
+# площади и периметра). При потребности создавать все необходимые
+# методы не описанные в задании. Создать список фигур и в цикле
+# подсчитать и вывести площади всех фигур на экран[my-oop-03]
+# Примечание: в рамках задание создать два файла: classes.py и main.py. В
+# первом будут описаны все классы, во втором классы будут
+# импортированы и использованы.
+
+from abc import ABC, abstractmethod
+import math
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+class AbstractFigure(ABC):
+
+    @abstractmethod
+    def find_an_area(self):
+        pass
+
+    @abstractmethod
+    def find_a_perimetr(self):
+        pass
+
+    @staticmethod
+    def find_two_points_len(point1: "Point", point2: "Point"):
+        return math.sqrt((point2.y - point1.y) ** 2 + (point2.x - point1.x) ** 2)
+
+
+class Circle(AbstractFigure):
+    def __init__(self, center: "Point", point_on_circle: "Point" = None, radius: float = None):
+        self.center = center
+        self.point_on_circle = point_on_circle
+        self.radius = radius
+
+    def find_an_area(self):
+        if self.radius:
+            print(1)
+            return math.pi * self.radius ** 2
+        else:
+            rad = self.find_two_points_len(self.center, self.point_on_circle)
+            return math.pi * rad ** 2
+
+    def find_a_perimetr(self):
+        if self.radius:
+            return math.pi * self.radius * 2
+        else:
+            rad = self.find_two_points_len(self.center, self.point_on_circle)
+            return math.pi * rad * 2
+
+    def __str__(self):
+        area = self.find_an_area()
+        perimetr = self.find_a_perimetr()
+        return f"Circle with square {area} units and lenth {perimetr} units."
+
+
+class TriangleException(Exception):
+    def __init__(self, message='There are no such triangles'):
+         super().__init__(message)
+
+
+class Triangle(AbstractFigure):
+    def __init__(self, point1: "Point", point2: "Point", point3: "Point"):
+        self.point1 = point1
+        self.point2 = point2
+        self.point3 = point3
+        self.side_a = self.find_two_points_len(self.point1, self.point2)
+        self.side_b = self.find_two_points_len(self.point3, self.point2)
+        self.side_c = self.find_two_points_len(self.point1, self.point3)
+        if not(self.side_a + self.side_b > self.side_c and self.side_a +
+               self.side_c > self.side_b and self.side_b + self.side_c > self.side_a):
+            raise TriangleException
+        self.sides_array = sorted([self.side_a, self.side_b, self.side_c])
+        if self.sides_array[0]**2 + self.sides_array[1]**2 == self.sides_array[2]**2:
+            self.triangle_property = "Rectangular"
+        elif self.side_a == self.side_b == self.side_c:
+            self.triangle_property = "Equilateral"
+        elif self.side_a == self.side_b or self.side_b == self.side_c or self.side_a == self.side_c:
+            self.triangle_property = "Isosceles"
+        elif self.sides_array[0]**2 + self.sides_array[1]**2 < self.sides_array[2]**2:
+            self.triangle_property = "Obtuse" #тупой
+        else:
+            self.triangle_property = "Acute-angled" #острый
+
+    def find_a_perimetr(self):
+        return self.side_a + self.side_b + self.side_c
+
+    def find_an_area(self):
+        p = self.find_a_perimetr()/2
+        return math.sqrt(p*(p-self.side_a)*(p-self.side_b)*(p-self.side_c))
+
+    def __str__(self):
+        area = self.find_an_area()
+        perimetr = self.find_a_perimetr()
+        return f"{self.triangle_property} triangle with area {area} units and perimetr {perimetr} units."
+
+
+class Square(AbstractFigure):
+    def __init__(self, point1: "Point", point2: "Point"):
+        self.point1 = point1
+        self.point2 = point2
+        self.side = self.find_two_points_len(self.point1, self.point2)
+
+    def find_a_perimetr(self):
+        return self.side*4
+
+    def find_an_area(self):
+        return self.side**2
+
+    def __str__(self):
+        area = self.find_an_area()
+        perimetr = self.find_a_perimetr()
+        return f"Square with area {area} units and perimetr {perimetr} units."
