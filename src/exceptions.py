917594ac54711b3b8b46ac190bc60e5efa466b48
KarolinaLeonovich <karolinaleonovich@gmail.com>
2021-06-30 20:30:38 +0300

    Add noinput error

--- src/exceptions.py
+++ src/exceptions.py
@@ -6,3 +6,7 @@ class InputException(Exception):
 class DivByZero(Exception):
     def __init__(self, message='Cannot divide by zero!'):
         super().__init__(message)
+
+class NoInput(Exception):
+    def __init__(self, message='Enter numbers first!'):
+        super().__init__(message)
--- src/func.py
+++ src/func.py
@@ -1,46 +1,63 @@
 from exceptions import DivByZero
 from exceptions import InputException
+from exceptions import NoInput
 
 
 def add_operation_func(num1, num2):
     try:
         if num1.isalpha() or num2.isalpha():
             raise InputException
+        elif not num1 or not num2:
+            raise NoInput
         else:
             return int(num1) + int(num2)
     except InputException as i_e:
         return i_e
+    except NoInput as n_i:
+        return n_i
 
 
 def minus_operation_func(num1, num2):
     try:
         if num1.isalpha() or num2.isalpha():
             raise InputException
+        elif not num1 or not num2:
+            raise NoInput
         else:
             return int(num1) - int(num2)
     except InputException as i_e:
         return i_e
+    except NoInput as n_i:
+        return n_i
 
 
 def mult_operation_func(num1, num2):
     try:
         if num1.isalpha() or num2.isalpha():
             raise InputException
+        elif not num1 or not num2:
+            raise NoInput
         else:
             return int(num1) * int(num2)
     except InputException as i_e:
         return i_e
+    except NoInput as n_i:
+        return n_i
 
 
 def div_operation_func(num1, num2):
     try:
         if num2 == "0":
             raise DivByZero
-        if num1.isalpha() or num2.isalpha():
+        elif num1.isalpha() or num2.isalpha():
             raise InputException
+        elif not num1 or not num2:
+            raise NoInput
         else:
             return int(num1) / int(num2)
     except DivByZero as d_b_z:
         return d_b_z
     except InputException as i_e:
         return i_e
+    except NoInput as n_i:
+        return n_i
