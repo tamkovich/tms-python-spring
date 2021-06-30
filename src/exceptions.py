39eec211b5f1a5d8cd305d715a64a219277bd6d8
KarolinaLeonovich <karolinaleonovich@gmail.com>
2021-06-30 20:31:47 +0300

    Flake fixes n3

--- src/exceptions.py
+++ src/exceptions.py
@@ -7,6 +7,7 @@ class DivByZero(Exception):
     def __init__(self, message='Cannot divide by zero!'):
         super().__init__(message)
 
+
 class NoInput(Exception):
     def __init__(self, message='Enter numbers first!'):
         super().__init__(message)
