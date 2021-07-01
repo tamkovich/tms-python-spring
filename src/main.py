e7e0d6ec22efbed29ccd729ec65eaa6aa58306d3
Dkfore <dimonkubrak@gmail.com>
2021-07-01 14:09:09 +0300

    HW12: Dmitry Kubrak

---
+++ src/main.py
@@ -0,0 +1,24 @@
+from classes import Point, Triangle, Square, Circle
+
+
+"""Circle"""
+p = Point(0, 1)
+ci = Circle(p, 5)
+
+
+"""Triangle"""
+a = Point(0, 0)
+b = Point(2, 4)
+c = Point(3, 1)
+t = Triangle(a, b, c)
+
+
+"""Square"""
+point_a = Point(0, 1)
+point_b = Point(3, 3)
+s = Square(point_a, point_b)
+
+list1 = [ci, t, s]
+for x in list1:
+    print(x.area())
+    print(x.perimeter())
