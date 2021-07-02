0ae9f9b0f2008e0a36cce800db99c864e00bb741
KarolinaLeonovich <karolinaleonovich@gmail.com>
2021-07-02 19:05:14 +0300

    flake fix 1

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@
 # вычитание, умножение, деление). Добавить валидацию входных данных.
 # Программа должна состоять из четырех файлов(main.py, classes.py,
 # ui_func.py exceptions.py).
-from ui_func import ui_func
 from classes import Math
+from ui_func import ui_func
 
 
 flag = True
