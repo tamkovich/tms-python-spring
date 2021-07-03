faf160210fc14f7dab09cc4d6d6930184be4b496
yosu <yosu_95@inbox.ru>
2021-07-03 23:27:59 +0500

    HW13_01: Kasimova Yosuman

--- src/func.py
+++ src/func.py
@@ -18,5 +18,5 @@ def fun_multiplication(num1, num2):
 
 def fun_division(num1, num2):
     if exceptions.fun_TypeError(num1, num2, flag=0) != 1 and \
-            exceptions.fun_ZeroDivisionError(num1, num2, flag =0) != 1:
+            exceptions.fun_ZeroDivisionError(num1, num2, flag=0) != 1:
         return round((int(num1) / int(num2)), 3)
