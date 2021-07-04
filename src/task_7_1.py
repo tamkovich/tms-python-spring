676042693e0ad2eccabaa17128319c69ba7f736b
yosu <yosu_95@inbox.ru>
2021-07-04 21:44:25 +0500

    HW7: Kasimova Yosuman

--- src/task_7_1.py
+++ src/task_7_1.py
@@ -55,84 +55,37 @@ def fun_pinti(pint):  # Пинты в литры
 def fun_litr_pint(litr_pint):  # Литры в пинты
     return litr_pint * 2.11338
 
-
 while True:
-    print("1. Дюймы в сантиметры")
-    print("2. Сантиметры в дюймы")
-    print("3. Мили в километры")
-    print("4. Километры в мили")
-    print("5. Фунты в килограммы")
-    print("6. Килограммы в фунты")
-    print("7. Унции в граммы")
-    print("8. Граммы в унции")
-    print("9. Галлон в литры")
-    print("10. Литры в галлоны")
-    print("11. Пинты в литры")
-    print("12. Литры в пинты")
-    print("Выберите вариант перевода:  ")
-    trans = int(input())
-    if trans == 1:
-        print("Введите число для перевода: ")
-        number = input()
-        if number.isdigit():
-            print(f"Дюймы в сантиметры: {fun_inch(int(number))}")
-    if trans == 2:
-        print("Введите число для перевода: ")
-        number = input()
-        if number.isdigit():
-            print(f"Сантиметры в дюймы: {fun_santimetr(int(number))}")
-    if trans == 3:
-        print("Введите число для перевода: ")
-        number = input()
-        if number.isdigit():
-            print(f"Мили в километры: {fun_milli(int(number))}")
-    if trans == 4:
-        print("Введите число для перевода: ")
-        number = input()
-        if number.isdigit():
-            print(f"Километры в мили: {fun_kilo(int(number))}")
-    if trans == 5:
-        print("Введите число для перевода: ")
-        number = input()
-        if number.isdigit():
-            print(f"Фунты в килограммы: {fun_funt(int(number))}")
-    if trans == 6:
-        print("Введите число для перевода: ")
-        number = input()
-        if number.isdigit():
-            print(f"Килограммы в фунты: {fun_kilogram(int(number))}")
-    if trans == 7:
-        print("Введите число для перевода: ")
-        number = input()
-        if number.isdigit():
-            print(f"Унции в граммы: {fun_unsiya(int(number))}")
-    if trans == 8:
-        print("Введите число для перевода: ")
-        number = input()
-        if number.isdigit():
-            print(f"Граммы в унции: {fun_gramm(int(number))}")
-    if trans == 9:
-        print("Введите число для перевода: ")
-        number = input()
-        if number.isdigit():
-            print(f"Галлон в литры: {fun_galon(int(number))}")
-    if trans == 10:
-        print("Введите число для перевода: ")
-        number = input()
-        if number.isdigit():
-            print(f"Литры в галлоны: {fun_litr(int(number))}")
-    if trans == 11:
-        print("Введите число для перевода: ")
-        number = input()
-        if number.isdigit():
-            print(f"Пинты в литры: {fun_pinti(int(number))}")
-    if trans == 12:
-        print("Введите число для перевода: ")
-        number = input()
-        if number.isdigit():
-            print(f"Литры в пинты: {fun_litr_pint(int(number))}")
-    print("Для выхода из программы нажмите на ноль - '0")
-    trans = input()
-    if trans.isdigit():
-        if int(trans) == 0:
-            break
+    print("Введите число для перевода: ")
+    number = input()
+    if number.isdigit():
+        list_of_trans = {
+            1: fun_inch(int(number)),
+            2: fun_santimetr(int(number)),
+            3: fun_milli(int(number)),
+            4: fun_kilo(int(number)),
+            5: fun_funt(int(number)),
+            6: fun_kilogram(int(number)),
+            7: fun_unsiya(int(number)),
+            8: fun_gramm(int(number)),
+            9: fun_galon(int(number)),
+            10: fun_litr(int(number)),
+            11: fun_pinti(int(number)),
+            12: fun_litr_pint(int(number))
+        }
+        print("1. Дюймы в сантиметры")
+        print("2. Сантиметры в дюймы")
+        print("3. Мили в километры")
+        print("4. Километры в мили")
+        print("5. Фунты в килограммы")
+        print("6. Килограммы в фунты")
+        print("7. Унции в граммы")
+        print("8. Граммы в унции")
+        print("9. Галлон в литры")
+        print("10. Литры в галлоны")
+        print("11. Пинты в литры")
+        print("12. Литры в пинты")
+        print("Выберите вариант перевода:  ")
+        trans = int(input())
+        if trans in list_of_trans:
+            print(round(list_of_trans[trans], 3))
