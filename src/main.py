7f29eedb595eb9a05759c19a5c92702e7f0004b9
KarolinaLeonovich <karolinaleonovich@gmail.com>
2021-07-05 22:46:26 +0300

    HW14-01_KarolinaLeonovich

---
+++ src/main.py
@@ -0,0 +1,20 @@
+# Написать программу таймер. Программа при запуске принимает имя,
+# фамилию, часы, минуты и секунды. После программа начинает обратный
+# отсчет выводя оставшееся время. Программа должна хранить файл
+# логирования с информацией о том кто запускал программу и когда.
+# Пример:
+# 00:00:03
+# 00:00:02
+# 00:00:01
+# ALARM!!!
+from ui import TimerUI
+from classes import Timer
+from classes import User
+
+timer1 = TimerUI()
+
+user1 = User(timer1.name_back_input())
+user1.log_user()
+
+timer1 = Timer(*timer1.time_back_input())
+timer1.timer()
---
+++ src/ui.py
@@ -0,0 +1,21 @@
+import argparse
+
+
+class TimerUI:
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--name', required=True)
+        parser.add_argument('--hours', required=True)
+        parser.add_argument('--minutes', required=True)
+        parser.add_argument('--seconds', required=True)
+        self.args = parser.parse_args()
+
+    def name_back_input(self):
+        return self.args.name
+
+    def time_back_input(self):
+        return int(self.args.hours), int(self.args.minutes), int(self.args.seconds)
+
+
+#timer1 = TimerUI()
+#print(timer1.name_back_input())
