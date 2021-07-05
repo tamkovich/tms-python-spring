1f3bc9a3280254831151aa9ee487416116d5452f
Nikita Semiachka <82967086+Nikasem@users.noreply.github.com>
2021-07-05 10:44:34 +0300

    Update task_4_4.py

--- src/task_4_4.py
+++ src/task_4_4.py
@@ -3,13 +3,13 @@
 list = [1, 2, 3, 4, 5]
 new_list = []
 for i in list:
-    new_list.insert(len(new_list)-1, i)
+    new_list.insert(len(new_list) - 1, i)
 print(new_list)
 
 list_1 = [1, 2, 3, 4, 5]
 new_list_1 = []
 k = 1
 while k <= len(list_1):
-    new_list_1.insert(len(new_list_1)-1, k)
+    new_list_1.insert(len(new_list_1) - 1, k)
     k += 1
 print(new_list_1)
