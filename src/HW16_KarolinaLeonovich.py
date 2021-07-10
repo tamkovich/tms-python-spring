c57d6630ce6c4921beb66ed37a53f1d6c9c9445f
KarolinaLeonovich <karolinaleonovich@gmail.com>
2021-07-10 20:24:08 +0300

    Flake fixes

--- src/HW16_KarolinaLeonovich.py
+++ src/HW16_KarolinaLeonovich.py
@@ -1,9 +1,12 @@
 # Создать таблицы Brand(name),
-# Car(model, release_year, brand (foreing key на таблицу Brand)).
+# Car(model, release_year, brand (foreign key на таблицу Brand)).
 # Реализовать CRUD(создание, чтение, обновление по id, удаление по id)
 # для бренда и машины. Создать пользовательский интерфейс.
 
+
+import csv
 import sqlite3
+
 
 connection = sqlite3.connect("HW_16.db")
 
@@ -17,13 +20,12 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS car (id INTEGER PRIMARY KEY AUTOINC
                                                            col_model TEXT,
                                                            col_release_year INTEGER,
                                                            col_car_brand TEXT,
-                                                           FOREIGN KEY(col_car_brand) 
+                                                           FOREIGN KEY(col_car_brand)
                                                            REFERENCES brand(col_brand))''')
 
 # Записываем в таблицу brand брэнды из
 # какого-то скаченого csv из интернета
 
-import csv
 
 with open('csv.csv', newline='') as File:
     reader = csv.reader(File, delimiter=";")
@@ -31,7 +33,7 @@ with open('csv.csv', newline='') as File:
     for row in reader:
         if row[0] not in list_brands:
             list_brands.append(row[0])
-            cursor.execute(f'''INSERT INTO brand
+            cursor.execute('''INSERT INTO brand
                            (col_brand)
                            VALUES (?)''', (row[0],))
             connection.commit()
@@ -90,7 +92,3 @@ for i in k:
     print(s)
 
 # круд делать не буду потому что уже делала
-
-
-
-
