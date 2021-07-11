d3b42f35d70ecc8df6d5d993b9fcd13d56a16be1
pevlens <pevlens1501@gmail.com>
2021-07-11 15:07:06 +0300

    HW14_02:Serei Poluyan update_2

--- src/task_14_2.py
+++ src/task_14_2.py
@@ -7,7 +7,7 @@ import time
 
 def print_timer(second):
     s1 = second
-    while True:   
+    while True:
         hours = s1 / 60 / 60
         minuts = (hours - int(hours)) * 60
         seconds = s1 - int(minuts) * 60 - int(hours) * 60 * 60
@@ -19,15 +19,16 @@ def print_timer(second):
             s1 -= 1
             time.sleep(0.98)
 
+
 """Вызываем текущее врямя для записи запуска скрипта в лог файл"""
 time_now = datetime.datetime.now()
 
 """Вызываем обект argpsrse и задаемм аргументы при запуске скипта"""
 parser = argparse.ArgumentParser()
 parser.add_argument("-ln", "--last_name")
-parser.add_argument("-mf", "--minuts_focus", default= 25,  type=int)
-parser.add_argument("-mb", "--minuts_brk", default= 5,  type=int)
-parser.add_argument("-ct", "--count", default= 4,  type=int)
+parser.add_argument("-mf", "--minuts_focus", default=25, type=int)
+parser.add_argument("-mb", "--minuts_brk", default=5, type=int)
+parser.add_argument("-ct", "--count", default=4, type=int)
 parser.add_argument("-nt", "--name_task")
 arg = parser.parse_args()
 
@@ -39,8 +40,8 @@ count = arg.count
 while True:
     if count == 1:
         print_timer(arg.minuts_focus * 60)
-       
-    elif count != 0 :
+
+    elif count != 0:
         print_timer(arg.minuts_focus * 60)
         print_timer(arg.minuts_brk * 60)
     else:
