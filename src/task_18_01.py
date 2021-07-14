1333f07442ad3046a44ef21746aa8da0677e4198
Dmitry Kolyago <kaliaha@icloud.com>
2021-07-14 22:18:05 +0300

    HW18: Dmitry Kolyago

---
+++ src/task_18_01.py
@@ -0,0 +1,50 @@
+from classes import SQLiteDataBase
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    global db, table
+    return render_template('index.html', all_items=db.get_all_items(table))
+
+
+@app.route('/edit/<int:item_id>', methods=['POST', 'GET'])
+def edit(item_id: int):
+    global db, table
+
+    if request.method == 'POST':
+        if request.form.get('delete'):
+            db.delete_item_by_id(table, item_id)
+        else:
+            name = request.form.get('name')
+            price = request.form.get('price')
+            count = request.form.get('count')
+            comment = request.form.get('comment')
+            db.update_item_by_id(table, item_id,
+                                 name=name, price=price, count=count, comment=comment)
+
+        return render_template('index.html', all_items=db.get_all_items(table))
+    return render_template('edit.html', item=db.get_item_by_id(table, item_id=item_id))
+
+
+@app.route('/add/', methods=['POST', 'GET'])
+def add():
+    global db, table
+    if request.method == 'POST':
+        name = request.form.get('name')
+        price = request.form.get('price')
+        count = request.form.get('count')
+        comment = request.form.get('comment')
+        db.add_item(table, name=name, price=price, count=count, comment=comment)
+        return render_template('index.html', all_items=db.get_all_items(table))
+    return render_template('add.html')
+
+
+if __name__ == '__main__':
+    db = SQLiteDataBase('products.db')
+    table = 'products'
+    app.run()
