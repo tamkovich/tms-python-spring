3b04877eb82ad77f75377ac96c9ea546ff2201d7
SerG-pyt <siarheish@gmail.com>
2021-07-21 13:08:30 +0300

    HW9: Сергей Швайковский

--- src/task_9_3.py
+++ src/task_9_3.py
@@ -10,9 +10,9 @@ from random import randint
 def del_even_num(real_func):
     def fake_func(*args: list) -> list:
         """Ф-ция проверяет четность списка list_num, формирует и возвращает список нечетных"""
-        res = filter(lambda x: x % 2 != 0, real_func(*args))
-        print(f"Список нечетных чисел: {list(res)}")
-        return list(res)
+        res = list(filter(lambda x: x % 2 != 0, real_func(*args)))
+        print(f"Список нечетных чисел: {res}")
+        return res
 
     return fake_func
 
