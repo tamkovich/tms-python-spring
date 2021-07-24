0d4528f64cb78d5b2144e23814682d0319c019b5
Nikita Semiachka <82967086+Nikasem@users.noreply.github.com>
2021-07-24 10:45:18 +0300

    Update task_4_1.py

--- src/task_4_1.py
+++ src/task_4_1.py
@@ -1,16 +1,21 @@
 # 1) Дан список целых чисел.
 # Создать новый список, каждый элемент которого равен исходному элементу
 # умноженному на -2
-list = [1, 8, 34, 12]
+lst = [1, 8, 34, 12]
+new_lst = []
 i = 0
-for _ in list:
-    list[i] *= (-2)
+for _ in lst:
+    p = lst[i] * (-2)
     i += 1
-print(list)
+    new_lst.append(p)
+print(new_lst)
 
-list_1 = [1, 8, 34, 12]
+lst_1 = [1, 8, 34, 12]
+new_lst_1 = []
 k = 0
-while k < len(list_1):
-    list_1[k] *= (-2)
+while k < len(lst_1):
+    o = lst_1[k] * (-2)
     k += 1
-print(list_1)
+    new_lst_1.append(o)
+print(new_lst_1)
+
