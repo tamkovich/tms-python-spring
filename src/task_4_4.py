d15944f0e35299eb01048fb658d9d126808346ed
Nikita Semiachka <82967086+Nikasem@users.noreply.github.com>
2021-07-24 11:12:27 +0300

    Update task_4_4.py

--- src/task_4_4.py
+++ src/task_4_4.py
@@ -1,15 +1,15 @@
 # 4) Дан список. Создать новый список, сдвинутый на 1 элемент влево
 # Пример: 1 2 3 4 5 ->  2 3 4 5 1
-list = [1, 2, 3, 4, 5]
-new_list = []
-for i in list:
-    new_list.insert(len(new_list) - 1, i)
-print(new_list)
+lst = [1, 2, 3, 4, 5]
+new_lst = []
+for i in lst:
+    new_lst.insert(len(new_lst)-1, i)
+print(new_lst)
 
-list_1 = [1, 2, 3, 4, 5]
-new_list_1 = []
+lst_1 = [1, 2, 3, 4, 5]
+new_lst_1 = []
 k = 1
-while k <= len(list_1):
-    new_list_1.insert(len(new_list_1) - 1, k)
+while k <= len(lst_1):
+    new_lst_1.insert(len(new_lst_1)-1, k)
     k += 1
-print(new_list_1)
+print(new_lst_1)
