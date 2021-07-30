5e37585520425681a24079dd3bc5062c27922267
Yosuman <yosu_95@inbox.ru>
2021-07-30 14:01:34 +0500

    HW11: Kasimova Yosuman

--- src/task_10_1.py
+++ src/task_10_1.py
@@ -13,42 +13,42 @@ b - ширина прямоугольника
 """
 
 
-class fun_square:
-    def __init__(self, name, a, b):
+class FunSquare:
+    def __init__(self, name: str, a: float, b: float) -> None:
         self.__name = name
         self.__a = a
         self.__b = b
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str) -> None:
         self.__name = name
 
     @property
-    def a(self):
+    def a(self) -> float:
         return self.__a
 
     @a.setter
-    def a(self, a):
+    def a(self, a: float) -> None:
         self.__a = a
 
     @property
-    def b(self):
+    def b(self) -> float:
         return self.__b
 
     @b.setter
-    def b(self, b):
+    def b(self, b: float) -> None:
         self.__b = b
 
     # функция площадь прямоугольника
-    def area(self, a, b):
+    def area(self, a: float, b: float) -> float:
         return a * b
 
     # функция периметр прямоугольника
-    def perimeter(self, a, b):
+    def perimeter(self, a: float, b: float) -> float:
         return 2 * (a + b)
 
 
@@ -60,42 +60,42 @@ h - высота цилиндра
 """
 
 
-class fun_cylinder:
-    def __init__(self, name, r, h):
+class FunCylinder:
+    def __init__(self, name: str, r: float, h: float) -> None:
         self.__name = name
         self.__r = r
         self.__h = h
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str) -> None:
         self.__name = name
 
     @property
-    def r(self):
+    def r(self) -> float:
         return self.__r
 
     @r.setter
-    def r(self, r):
+    def r(self, r: float) -> None:
         self.__r = r
 
     @property
-    def h(self):
+    def h(self) -> float:
         return self.__h
 
     @h.setter
-    def h(self, h):
+    def h(self, h: float) -> None:
         self.__h = h
 
     # Нахождения площадь цилиндра
-    def area(self, r, h):
+    def area(self, r: float, h: float) -> float:
         return 2 * r * r * 3.14 + 2 * r * h * 3.14
 
     # Нахождения объем цилиндра
-    def volume(self, r):
+    def volume(self, r: float) -> float:
         return r * r * 3.14
 
 
@@ -108,51 +108,51 @@ h - высота трапеции
 """
 
 
-class fun_trapezoid:
-    def __init__(self, name, a, b, h):
+class FunTrapezoid:
+    def __init__(self, name: str, a: float, b: float, h: float) -> None:
         self.__name = name
         self.__a = a
         self.__b = b
         self.__h = h
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str) -> None:
         self.__name = name
 
     @property
-    def a(self):
+    def a(self) -> float:
         return self.__a
 
     @a.setter
-    def a(self, a):
+    def a(self, a: float) -> None:
         self.__a = a
 
     @property
-    def b(self):
+    def b(self) -> float:
         return self.__b
 
     @b.setter
-    def b(self, b):
+    def b(self, b: float) -> None:
         self.__b = b
 
     @property
-    def h(self):
+    def h(self) -> float:
         return self.__h
 
     @h.setter
-    def h(self, h):
+    def h(self, h: float) -> None:
         self.__h = h
 
     # Площадь трапеции
-    def area(self, a, b, h):
+    def area(self, a: float, b: float, h: float) -> float:
         return (h * (a + b)) / 2
 
     #  Нахождении середини линии трапеции
-    def middle_line(self, a, b):
+    def middle_line(self, a: float, b: float) -> float:
         return (a + b) / 2
 
 
@@ -165,51 +165,51 @@ c - третья сторона
 """
 
 
-class fun_triangle:
-    def __init__(self, name, a, b, c):
+class FunTriangle:
+    def __init__(self, name: str, a: float, b: float, c: float) -> None:
         self.__name = name
         self.__a = a
         self.__b = b
         self.__c = c
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str) -> None:
         self.__name = name
 
     @property
-    def a(self):
+    def a(self) -> float:
         return self.__a
 
     @a.setter
-    def a(self, a):
+    def a(self, a: float) -> None:
         self.__a = a
 
     @property
-    def b(self):
+    def b(self) -> float:
         return self.__b
 
     @b.setter
-    def b(self, b):
+    def b(self, b: float) -> None:
         self.__b = b
 
     @property
-    def c(self):
+    def c(self) -> float:
         return self.__c
 
     @c.setter
-    def c(self, c):
+    def c(self, c: float) -> None:
         self.__c = c
 
     # Нахождения площадь треугольника
-    def area(self, a, b, c, p):
+    def area(self, a: float, b: float, c: float, p: float) -> float:
         return (p * (p - a) * (p - b) * (p - c)) ** 1 / 2
 
     # Нахождения периметра треугольника
-    def perimeter(self, a, b, c):
+    def perimeter(self, a: float, b: float, c: float) -> float:
         return (a + b + c) / 2
 
 
@@ -221,67 +221,67 @@ h - высота рома
 """
 
 
-class fun_rhombus:
-    def __init__(self, name, a, h):
+class FunRhombus:
+    def __init__(self, name: str, a: float, h: float) -> None:
         self.__name = name
         self.__a = a
         self.__h = h
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str) -> None:
         self.__name = name
 
     @property
-    def a(self):
+    def a(self) -> float:
         return self.__a
 
     @a.setter
-    def a(self, a):
+    def a(self, a: float) -> None:
         self.__a = a
 
     @property
-    def h(self):
+    def h(self) -> float:
         return self.__h
 
     @h.setter
-    def h(self, h):
+    def h(self, h: float) -> None:
         self.__h = h
 
     # Нахождения площадь ромба
-    def area(self, a, h):
+    def area(self, a: float, h: float) -> float:
         return a * h
 
     # Периметр ромба
-    def perimeter(self, a):
+    def perimeter(self, a: float) -> float:
         return 4 * a
 
 
-square = fun_square("parallelogram", 6, 12)
+square = FunSquare("parallelogram", 6, 12)
 print(square.name)
 print(f"parallelogram area = {square.area(square.a, square.b)}")
 print(f"parallelogram perimeter = {square.perimeter(square.a, square.b)}")
 print("**************")
-cylinder = fun_cylinder("cylinder", 6, 12)
+cylinder = FunCylinder("cylinder", 6, 12)
 print(cylinder.name)
 print(f"cylinder area = {cylinder.area(cylinder.r, cylinder.h)}")
 print(f"cylinder volume = {cylinder.volume(cylinder.r)}")
 print("**************")
-trapezoid = fun_trapezoid("trapezoid", 6, 12, 8)
+trapezoid = FunTrapezoid("trapezoid", 6, 12, 8)
 print(trapezoid.name)
 print(f"trapezoid area = {trapezoid.area(trapezoid.a, trapezoid.b, trapezoid.h)}")
 print(f"trapezoid middle_line = {trapezoid.middle_line(trapezoid.a, trapezoid.b)}")
 print("**************")
-triangle = fun_triangle("triangle", 6, 12, 8)
+triangle = FunTriangle("triangle", 6, 12, 8)
 print(triangle.name)
 p = triangle.perimeter(triangle.a, triangle.b, triangle.c)
 print(f"triangle area = {triangle.area(triangle.a, triangle.b, triangle.c, p)}")
 print(f"triangle perimeter = {p}")
 print("**************")
-rhombus = fun_rhombus("rhombus", 6, 8)
+rhombus = FunRhombus("rhombus", 6, 8)
 print(rhombus.name)
 print(f"rhombus area = {rhombus.area(rhombus.a, rhombus.h)}")
 print(f"rhombus perimeter = {rhombus.perimeter(rhombus.a)}")
