eee6e27b94bf17a69754674b81c688d906b90e72
Nikita Semiachka <82967086+Nikasem@users.noreply.github.com>
2021-08-11 10:11:37 +0300

    Update task_7_02.py

--- src/task_7_02.py
+++ src/task_7_02.py
@@ -1,6 +1,8 @@
-# Задание 7.02 (HW)
-# Написать программу для работы с матрицами:
-# 1)Создание
+"""Задание 7.02 (HW)
+Написать программу для работы с матрицами:"""
+"""1)Создание"""
+
+
 import random
 
 
@@ -15,29 +17,35 @@ def create_matrix(n):
     return matrix
 
 
-# 2) Вывод
+"""2) Вывод"""
+
+
 def print_matrix(matrix):
     for row in matrix:
         print(row)
 
 
-# 3)Сумма всех элементов
+"""3)Сумма всех элементов"""
+
+
 def sum_all_elements(matrix):
     list_of_all_elements = []
     for row in matrix:
         list_of_all_elements.append(sum(row))
     return sum(list_of_all_elements)
 
+"""4)Нахождение максимального элемента"""
 
-# 4)Нахождение максимального элемента
+
 def find_max_element(matrix):
     list_of_max_elements = []
     for row in matrix:
         list_of_max_elements.append(max(row))
     return max(list_of_max_elements)
 
+"""5)Нахождение минимального элемента"""
 
-# 5)Нахождение минимального элемента.
+
 def find_min_element(matrix):
     list_of_min_elements = []
     for row in matrix:
